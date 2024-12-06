8579d3e5b692b8e007aac3e0d0f6a1573d0045b8
Amprex5101 <al21020019@itsa.edu.mx>
2024-12-05 20:32:20 -0600

    CAMBIO DE NAVBAR

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 module.exports = function(modules) {
     router.get('/', (req, res) => {
         // Renderizar la vista pasando el contenido del cuerpo como parámetro
-        res.render(req.layout_view, { user:req.user, modules:modules, title: 'Home', bodyContent: './index.ejs',loadingPath: req.loadingPath,navbarPath: req.navbarPath });
+        res.render(req.layout_view, { user:req.user, modules:modules, title: 'Home', bodyContent: './Restaurantes.ejs',loadingPath: req.loadingPath,navbarPath: req.navbarPath });
     });
 
     return router;
